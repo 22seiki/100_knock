4bdb7466accaf8a21fa7c9e821df3a8b42023e12
MatobaSeiki <e1b15097@st.oit.ac.jp>
2018-06-05 21:29:55 +0900

    ファイル処理に修正

--- chapter2/2-10.py
+++ chapter2/2-10.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import subprocess
-cmd = ['wc', '-l', 'hightemp.txt']
-try:
-    res = subprocess.check_call(cmd)
-except:
-    print "Error."
+
+f = open("hightemp.txt", "r")
+lines = f.readlines()
+print(len(lines))
+f.close()
