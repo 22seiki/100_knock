15504b3fa77a0464e65bb02188d094d4253150c6
MATOBAD <e1b15097@st.oit.ac.jp>
2018-07-10 03:45:02 -0700

    not complete

---
+++ chapter5/47.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import collections
+import sys
+import re
+from functions import Chunk, Morph
+
+
+def make_sentences(text):
+    lines = []
+    sentences = []
+    global pos_list
+    for line in txt.split("\n"):
+        line = re.sub("[,|\t]", " ", line)
+        lists = list(line.split(" "))
+        if len(lists) > 5 and "EOS" not in lists:
+            dic = {
+                "surface": lists[0],
+                "base": lists[7],
+                "pos": lists[1],
+                "pos1": lists[2]
+            }
+            morph = Morph(dic['surface'], dic['base'], dic['pos'], dic['pos1'])
+            lines.append(dic)
+        elif "EOS" not in lists:
+            lines.append(lists)
+        if "EOS" in lists:
+            sentences.append(lines)
+            lines = []
+    return sentences
+
+
+def make_chunk(sentence):
+    chunk = Chunk()
+    chunk(sentence)
+    for lines in sentence:
+        if isinstance(lines, list):
+            s = {}
+            lines[2] = re.sub("D", "", lines[2])
+            chunk.dst = int(lines[2])
+            id = int(lines[1])
+            chunk.morphs.append([])
+        else:
+            if lines['pos'] == '動詞':
+                lines['base'] = re.sub("[。|、|\s]", "", lines['base'])
+                s = {lines['pos']: [lines['surface'], lines['base']]}
+            elif lines['pos1'] == 'サ変接続':
+                lines['pos1'] = re.sub("[。|、|\s]", "", lines['pos1'])
+                s = {lines['pos1']: lines['surface']}
+            else:
+                lines['surface'] = re.sub("[。|、|\s]", "", lines['surface'])
+                s = {lines['pos']: lines['surface']}
+            chunk.morphs[id].append(s)
+            if chunk.dst != -1 and id not in chunk.srcs[chunk.dst]:
+                chunk.srcs[chunk.dst].append(id)
+    return chunk
+
+
+def make_paterns(morph1, morph2):
+    print(morph1, morph2)
+    paterns = []
+    for i, vdic in enumerate(morph1):
+        if i < len(morph1):
+            if 'サ変接続' in vdic and '助詞' in morph1[i+1]:
+                if morph1[i+1]['助詞'] == 'を':
+                    print(morph1[i+1]['助詞'])
+                    s = vdic['サ変接続'] + morph1[i+1]['助詞']
+                    + morph1[i+2]['動詞'] + '\t'
+                    s2 = ''
+                    sv = ''
+                    flag = False
+                    for dic in morph2:
+                        if '動詞' not in dic:
+                            l = list(dic.values())
+                            sv += l[0]
+                        else:
+                            sv += dic['動詞'][0]
+                        if '助詞' in dic:
+                            s += dic['助詞'] + ' '
+                            s2 += sv + ' '
+                            sv = ''
+                            flag = True
+                    s += '\t' + s2
+                    if flag:
+                        paterns.append(s)
+    return paterns
+
+
+if __name__ == '__main__':
+    path = "./neko.txt.cabocha"
+    with open(path, "r") as f:
+        txt = str(f.read())
+    sentences = make_sentences(txt)
+    v_sentenses = []
+    for sentence in sentences:
+        chunk = make_chunk(sentence)
+        for i, src in enumerate(chunk.srcs):
+            if [] == src:
+                continue
+            for id in src:
+                paterns = make_paterns(chunk.morphs[i], chunk.morphs[id])
+                for patern in paterns:
+                    v_sentenses.append(patern)
+    counter = collections.Counter(v_sentenses)
+    lines = ''
+    for k in counter.keys():
+        lines += k + '\n'
+    with open('47result.txt', 'w') as f:
+        f.write(lines)
